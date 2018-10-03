f8cb23c5c8e97e54f2799da5bbf9ff8b4ca790a1
Andrew Innocent <innocent.andrew@gmail.com>
2018-10-02 23:18:04 -0400

    Add dynamic content within JSX

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App extends Component {
         <h1>Hi! I'm a react app!</h1>
         <p>This really works!</p>
         <Person />
+        <Person />
+        <Person />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App)) // Takes at least three parameters: 1. element to render to DOM 2. Config for element in JS 3. Children of element 4... content in element
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,8 @@
 import React from 'react'
 
 const person = () => {
-    return <p>I'm a Person!</p>
+    // Output dynamic content within JSX. It must be wrapped in {}.
+    return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
 }
 
 export default person;
