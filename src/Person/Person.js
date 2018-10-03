ea208c0c4ec8348ad02c69f65d6b02a40e718dc8
Andrew Innocent <innocent.andrew@gmail.com>
2018-10-02 22:05:44 -0400

    Creates a Person component

---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+// Convention is to capitalize file names for components
+// At its basic, a component is just a function that returns JSX
+import React from 'react'
+
+const person = () => {
+    return <p>I'm a Person!</p>
+}
+
+export default person;
