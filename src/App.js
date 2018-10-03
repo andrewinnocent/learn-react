e7b1e8bad6623c7c936e050b9e9615569acc162c
Andrew Innocent <innocent.andrew@gmail.com>
2018-10-02 21:50:19 -0400

    React.createElement(): How to add className to an element using as JS object

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     //     <h1>Hi! I'm a react app!</h1>
     //   </div>
     // );
-    return React.createElement('div', null, React.createElement('h1', null, 'Does this work now?')) // Takes at least three parameters: 1. element to render to DOM 2. Config for element in JS 3. Children of element 4... content in element
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?')) // Takes at least three parameters: 1. element to render to DOM 2. Config for element in JS 3. Children of element 4... content in element
   }
 }
 
