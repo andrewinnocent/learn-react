021742939116aee8ed6a8103268954d96d22a5ee
Andrew Innocent <innocent.andrew@gmail.com>
2018-10-02 21:42:47 -0400

    React.createElement()
    Method used to create an element to be rendered. This can also include a component.
    
    It takes at least 3 parameters: HTML element, behavior of element in JS, content in element.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi! I'm a react app!</h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hi! I'm a react app!</h1>
+    //   </div>
+    // );
+    return React.createElement('div', null, 'h1', ' Hi, I\'m learning JSX!!!', 'What?') // Takes at least three parameters: 1. element to render to DOM 2. Config for element in JS 3. Children of element 4... content in element
   }
 }
 
