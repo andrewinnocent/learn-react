87e023901584748643f028ef53a4a9ecca63f240
Andrew Innocent <innocent.andrew@gmail.com>
2018-10-02 21:52:47 -0400

    JSX is compiled to React.createElement()

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi! I'm a react app!</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?')) // Takes at least three parameters: 1. element to render to DOM 2. Config for element in JS 3. Children of element 4... content in element
+    return (
+      <div className="App">
+        <h1>Hi! I'm a react app!</h1>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App)) // Takes at least three parameters: 1. element to render to DOM 2. Config for element in JS 3. Children of element 4... content in element
   }
 }
 
