a98fd9107956249507bd61bfd0084e07e796b38f
Andrew Innocent <innocent.andrew@gmail.com>
2018-10-03 22:33:49 -0400

    Pass methods between components & binding

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Clicked!')
     // 'this' refers to the class because it's inside an arrow function. IMPORTANT!
     // DON'T DO THIS: this.state.persons[0].name = 'John'
     this.setState(
       {
         persons: [
-          {name: 'John', age: 32},
+          {name: newName, age: 32},
           {name: 'Rachelle', age: 25},
           {name: 'Drew', age: 21}
         ]
@@ -32,10 +32,18 @@ class App extends Component {
       <div className="App">
         <h1>Hi! I'm a react app!</h1>
         <p>This really works!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button> {/* NOTICE the function isn't being invoked (). If it was, it would execute as soon as component is rendered. This isn't the behavior desired here. */}
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Hiking</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('John')}>Switch Name</button> {/* The function is executed on the click instead of the page load. */}
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}/>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          // methods can be passed down to other components
+          click={this.switchNameHandler.bind(this, 'Johnny!')}>My Hobbies: Hiking</Person> {/* .bind() is the preferred way pass a function parameter over the arrow function. */}
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App)) // Takes at least three parameters: 1. element to render to DOM 2. Config for element in JS 3. Children of element 4... content in element
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,7 +6,7 @@ const person = (props) => {
     // Output dynamic content within JSX. It must be wrapped in {}.
     return (
         <div>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p> {/* children is a reserved word in JSX. This will access content placed between open/close tags of the component. */}
         </div>
     )
