43bca76603767a75b8ac73b5d0e72c873e8d9139
Andrew Innocent <innocent.andrew@gmail.com>
2018-10-03 21:14:28 -0400

    Introduces 'state' object in class-based components
    props and state  are CORE concepts of React. Actually, only changes in props  and/ or state  trigger React to re-render your components and potentially update the DOM in the browser
    
    'props' allow you to pass data from a parent (wrapping) component to a child (embedded) component.
    
    'state' is used to change the component, well, state from within.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  // 'state' (reserved word) is ONLY in classes that extend Component (class-based components).
+  state = {
+    persons: [
+      {name: 'Andrew', age: 32},
+      {name: 'Rachelle', age: 25},
+      {name: 'Drew', age: 30}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi! I'm a react app!</h1>
         <p>This really works!</p>
-        <Person name='Andrew' age='32'/>
-        <Person name='Rachelle' age='25'>My Hobbies: Hiking</Person>
-        <Person name='Drew' age='30'/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Hiking</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App)) // Takes at least three parameters: 1. element to render to DOM 2. Config for element in JS 3. Children of element 4... content in element
