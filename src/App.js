2595a355be2cd77a6b3742438837eb18638e8eba
Andrew Innocent <innocent.andrew@gmail.com>
2018-10-16 22:56:25 -0400

    Truly dynamically update components
    Introduces spread operator for objects also findIndex() vanilla JS method.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,21 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = (e, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {...this.state.persons[personIndex]}; // create a copy of state's person object
+
+    person.name = e.target.value; // update name from input field
+
+    const persons = [...this.state.persons]; // create a copy of state's persons array
+    persons[personIndex] = person; // update copied array with updated person object
+
+    this.setState({persons: persons}) // setState with new data
+  }
+
   deletePersonHandler = (personIndex) => {
     // ALWAYS update state in an immutable fashion (i.e., make a copy of state and manipulate the copy).
     // const persons = this.state.persons.slice();  // slice() makes a copy of the array
@@ -44,7 +59,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id} />
+              key={person.id}
+              changed={(e) => this.switchNameHandler(e, person.id)} />
           })}
           </div>
       )
