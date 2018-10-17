8d9f16d8314a75286a2954cd35fa743a53f13f1f
Andrew Innocent <innocent.andrew@gmail.com>
2018-10-16 21:45:47 -0400

    Introduces map()
    Vanilla JS array method. Useful to iterate over an array and return JSX to render.

--- src/App.js
+++ src/App.js
@@ -58,22 +58,15 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}/>
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Johnny!')}
-              changed={this.nameChangedHandler}>My Hobbies: Hiking</Person> {/* .bind() is preferred over the arrow function to pass a function parameter. */}
-              {/* Only this component has action handlers passed to it. */}
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}/>
+          {this.state.persons.map(person => {
+            return <Person 
+              name={person.name}
+              age={person.age} />
+          })}
           </div>
       )
     }
-    
+
     return (
       <div className="App">
         <h1>Hi! I'm a react app!</h1>
