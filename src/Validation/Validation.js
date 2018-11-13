89b6aa0ae04a5f52ccd5df5bc8a43cad1e2c01be
Andrew Innocent <innocent.andrew@gmail.com>
2018-11-13 16:42:03 -0500

    Styling using Radium

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 const validation = (props) => {
+	let validationMessage = <p>Word is too short.</p>
+
+	if (props.length > 5) {
+		validationMessage = <p>Word is long enough.</p>
+	}
+	
 	return (
 		<div>
-			{
+			{/* {
 				props.length > 5 ?
 				<p>Word is long enough.</p> :
 				<p>Word is too short.</p>
-			}
+			} */}
+			{validationMessage}
 		</div>
 	)
 }
