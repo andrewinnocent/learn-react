87e065a6331770d7be8f40bf740666320b814c4e
Andrew Innocent <innocent.andrew@gmail.com>
2018-11-19 16:29:30 -0500

    Prep for implementation of Lifecycle Hooks. Turns functional components to stateful containers

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,18 @@
 // Convention is to capitalize file names for components
 // At its basic, a component is just a function that returns JSX
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.module.css';
 
-const person = (props) => {
-    // Output dynamic content within JSX. It must be wrapped in {}.
-  
-    return (
-        <div className={classes.Person}>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p> {/* children is a reserved word in JSX. This will access content placed between open/close tags of the component. */}
-            <input type='text' onChange={props.changed} value={props.name}/> {/* Two-way binding: update the state (onChange) and see the current state (value) */}
-        </div>
-    )
+class Person extends Component {
+    render () {
+        return (
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p> {/* children is a reserved word in JSX. This will access content placed between open/close tags of the component. */}
+                <input type='text' onChange={this.props.changed} value={this.props.name}/> {/* Two-way binding: update the state (onChange) and see the current state (value) */}
+            </div>
+        )
+    }
 }
 
-export default person;
+export default Person;
